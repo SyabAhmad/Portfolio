bfe479b6f9638380ec30f26899b85fe8ac6dde30
SyabAhmad <syabblogger@gmail.com>
2023-07-23 15:36:37 +0500

    changes

--- Portfolio/src/Components/Hero2.jsx
+++ Portfolio/src/Components/Hero2.jsx
@@ -1,7 +1,43 @@
 import React from "react";
-
+import githubimage from "../assets/github.png";
+import "./Hero2.css";
 const Hero2 = () => {
-  return <>hero2</>;
+  return (
+    <>
+      <div className="hero2">
+        <div className="cardLarg">
+          <div>
+            LinkedIn
+            <br />
+            Connections
+            <br />
+            <span>1200+</span>
+          </div>
+          <div>
+            Instagram
+            <br />
+            Followers
+            <br />
+            <span>2500+</span>
+          </div>
+          <div>
+            GitHub
+            <br />
+            Followers
+            <br />
+            <span>4120+</span>
+          </div>
+          <div className="githubImage">
+            <img src={githubimage} alt="" />
+          </div>
+        </div>
+        <div className="recent">
+          <div className="details1"></div>
+          <div className="images"></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Hero2;
